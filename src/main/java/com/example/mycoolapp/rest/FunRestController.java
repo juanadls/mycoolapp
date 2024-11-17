5e26729216cd62d909569059e8c4b05f9605e073
juanadls <eleo.dev09@gmail.com>
2024-11-17 13:54:57 -0300

    added two new enpoints

--- src/main/java/com/example/mycoolapp/rest/FunRestController.java
+++ src/main/java/com/example/mycoolapp/rest/FunRestController.java
@@ -8,8 +8,18 @@ public class FunRestController {
 
     @GetMapping ("/")
     public  String sayHelloWorld(){
+        //expose "/"  that returns "Hello World"
         return "Hello World";
     }
+    //exposes a new endpoint for workout
+    @GetMapping ("/workout")
+    public String getDailyWorkout(){
+        return "Run a hard 5k!";
+    }
 
-    //expose / that returns Hello World
+    @GetMapping ("/fortune")
+        public String getDailyFortune(){
+            return "Today is your lucky day";
+    }
+
 }
