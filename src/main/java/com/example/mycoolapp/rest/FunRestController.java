c982dd110717e8573cabb43da4087cc8db6ccae5
juanadls <eleo.dev09@gmail.com>
2024-11-17 12:38:04 -0300

    added rest controller

--- src/main/java/com/example/mycoolapp/rest/FunRestController.java
+++ src/main/java/com/example/mycoolapp/rest/FunRestController.java
@@ -1,4 +1,15 @@
 package com.example.mycoolapp.rest;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class FunRestController {
+
+    @GetMapping ("/")
+    public  String sayHelloWorld(){
+        return "Hello World";
+    }
+
+    //expose / that returns Hello World
 }
